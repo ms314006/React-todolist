8dcd2fa81586ca5d29fcf7630e55e1e7b9f78004
GQSM <ms314006@hotmail.com>
2018-10-18 19:52:00 +0800

    add

--- step2/src/components/InputTasksForm/InputTasksForm.jsx
+++ step2/src/components/InputTasksForm/InputTasksForm.jsx
@@ -8,17 +8,17 @@ class InputTasksForm extends React.Component {
                 <div class="InputTask">
                     <InputName className="fas fa-calendar-alt" inputName="Deadline" />
                     <div class="inputForm">
-                        <input type="date" class="inputFont inputDateTime" />
+                        <input type="date" class="inputStyle inputDateTime" />
                         &nbsp;&nbsp;
-                        <input type="time" class="inputFont inputDateTime" />
+                        <input type="time" class="inputStyle inputDateTime" />
                     </div>
                     <InputName className="fas fa-file" inputName="File" />
                     <div class="inputForm">
-                        <input type="file" class="inputFont" />
+                        <input type="file" class="inputStyle" />
                     </div>
                     <InputName className="far fa-comment-dots" inputName="Comment" />
                     <div class="inputForm">
-                        <textarea rows="8" cols="55" class="inputFont"></textarea>
+                        <textarea rows="7" cols="55" class="inputStyle"></textarea>
                     </div>
                 </div>
                 <div>
