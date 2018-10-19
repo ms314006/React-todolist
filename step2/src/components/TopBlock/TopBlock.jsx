4dd89508109791cdb679974dfa1a3666fcd156c6
GQSM <ms314006@hotmail.com>
2018-10-19 17:44:17 +0800

    up -p

--- step2/src/components/TopBlock/TopBlock.jsx
+++ step2/src/components/TopBlock/TopBlock.jsx
@@ -5,7 +5,7 @@ class TopBlock extends React.Component{
     render(){
         return(
             <div id="topBlock">
-                <BookMark to="/" name="My Tasks" />
+                <BookMark to="/" name="My Tasks1" />
                 <BookMark to="/inProgress" name="In Progress" />
                 <BookMark to="/completed" name="Completed" />
             </div>
