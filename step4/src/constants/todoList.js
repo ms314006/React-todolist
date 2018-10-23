7a7c1573a6d95aa13cd1e5a3365828e6d77dfeb0
GQSM <ms314006@hotmail.com>
2018-10-23 20:46:48 +0800

    change data

--- step4/src/constants/todoList.js
+++ step4/src/constants/todoList.js
@@ -1,8 +1,8 @@
 const todoData = [
     {id:1,name:'鐵人賽文章',date:'2018-10-20',time:'23:00',file:'todolist結構.jpg',
-    commit:'今天要處理完資料的部分',important:'Y',complete:''},
+    commit:'今天要處理完資料的部分',important:'Y',complete:true},
     {id:2,name:'要去成大跑步',date:'2018-10-19',time:'20:00',file:'',
-    commit:'今天的目標是30分跑完6公里',important:'',complete:'Y'}
+    commit:'今天的目標是30分跑完6公里',important:'',complete:true}
 ]
 
 export {todoData}
