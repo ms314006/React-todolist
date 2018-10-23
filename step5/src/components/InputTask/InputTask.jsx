956778b3aa571ddcb953fcbf7b1a6c2001ef7e77
GQSM <ms314006@hotmail.com>
2018-10-23 22:27:14 +0800

    up

--- step5/src/components/InputTask/InputTask.jsx
+++ step5/src/components/InputTask/InputTask.jsx
@@ -6,46 +6,50 @@ import { addTodoList } from "../../actions"
 class ConnectInputTask extends React.Component {
     constructor(props){
         super(props)
-        this.state = {id:'',name:'',date:'',time:'',file:'',commit:'',important:'',complete:''}
+        this.state = {id:'',name:'',date:'',time:'',file:'',commit:'',important:'',complete:false}
         this.changeState = this.changeState.bind(this)
         this.submitTodo = this.submitTodo.bind(this)
         this.tagComplete = this.tagComplete.bind(this)
         this.tagImportant = this.tagImportant.bind(this)
         this.filebox = React.createRef()
+
+        this.star = React.createRef()
+        this.inputTasksTitle = React.createRef()
+        this.name = React.createRef()
     }
 
     tagComplete(event) {
         if (event.target.checked) {
-            document.getElementById(`name`).classList.add('complete')
-            this.setState({complete:'Y'})
+            this.name.current.classList.add('complete')
         }
         else {
-            document.getElementById(`name`).classList.remove('complete')
-            this.setState({complete:''})
+            this.name.current.classList.remove('complete')
         }
+        this.setState({complete:event.target.checked})
     }
 
-    tagImportant(event) {
+    tagImportant() {
         //取得目前icon的class名稱
-        let nowClass = event.target.className
-        //如果是空的星星
-        if (nowClass.indexOf('far fa-star') > -1) {
+        let star = this.star.current
+        let nowClass = star.className
+        //如果現在不是重要的就把它變重要的
+        if (this.state.important == '') {
             //就把星星變成填滿的icon
-            event.target.className = nowClass.replace('far fa-star', 'fas fa-star')
+            star.className = nowClass.replace('far fa-star', 'fas fa-star')
             //替星星的Icon增加class:iconImportant
-            event.target.classList.add('iconImportant')
+            star.classList.add('iconImportant')
             //替div和輸入框的背景增加class:important
-            document.getElementById('inputTasksTitle').classList.add("important")
-            document.getElementById('name').classList.add("important")
+            this.inputTasksTitle.current.classList.add("important")
+            this.name.current.classList.add("important")
             //改變important的狀態
             this.setState({important:'Y'})
         }
         else {
             //就上方的反過來
-            event.target.className = nowClass.replace('fas fa-star', 'far fa-star')
-            event.target.classList.remove('iconImportant')
-            document.getElementById('inputTasksTitle').classList.remove("important")
-            document.getElementById('name').classList.remove("important")
+            star.className = nowClass.replace('fas fa-star', 'far fa-star')
+            star.classList.remove('iconImportant')
+            this.inputTasksTitle.current.classList.remove("important")
+            this.name.current.classList.remove("important")
             this.setState({important:''})
         }
     }
@@ -69,22 +73,26 @@ class ConnectInputTask extends React.Component {
             //關閉新增畫面
             this.props.closeAdd()
             //清空資料
-            this.setState({id:'',name:'',date:'',time:'',file:'',commit:''})
+            this.setState({id:'',name:'',date:'',time:'',file:'',commit:'',important:'',complete:false})
             //不受控組件另外處理
             this.filebox.current.value = ''
+            //星星要點掉
+            this.tagImportant()
+            //刪除線的樣式也要拿掉
+            this.name.current.classList.remove('complete')
         }
     }
 
     render() {
         return (
             <div>
-                <div id="inputTasksTitle" class="inputTaskTitle">
-                    <input type="checkbox" class="taskChk" onChange={this.tagComplete} />
+                <div ref={this.inputTasksTitle} class="inputTaskTitle">
+                    <input type="checkbox" class="taskChk" checked={this.state.complete} onChange={this.tagComplete}  />
                     {/*替該name設定對應的state名稱，然後指定value為state中的值，和增加onChange事件，讓值改變時可以同時寫回`state`*/}
-                    <input id="name" name="name" type="text" class="taskTitle" placeholder="Type Something Here…" 
+                    <input ref={this.name} name="name" type="text" class="taskTitle" placeholder="Type Something Here…" 
                             value={this.state.name} 
                             onChange={this.changeState} />
-                    <i class="far fa-star fa-lg icon" onClick={this.tagImportant}></i>
+                    <i class="far fa-star fa-lg icon" onClick={this.tagImportant} ref={this.star}></i>
                     <i class="fas fa-pen fa-lg icon icon_edit"></i>
                 </div>
                 <InputTasksForm closeAdd={this.props.closeAdd}
