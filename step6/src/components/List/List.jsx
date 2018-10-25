cc6615771c94f040df0f66b2c809957e0b89dec2
GQSM <ms314006@hotmail.com>
2018-10-25 21:22:44 +0800

    調整list組件內容

--- step6/src/components/List/List.jsx
+++ step6/src/components/List/List.jsx
@@ -1,17 +1,13 @@
 import React from "react"
-import { InputTask } from "../InputTask"
 
 class List extends React.Component {
-
     constructor(props) {
         super(props)
         this.changeState = this.changeState.bind(this)
 
-        this.list = React.createRef()
         this.state = this.props.listData
 
     }
-
     changeState(type) {
         switch (type) {
             case "complete": {
@@ -30,39 +26,35 @@ class List extends React.Component {
 
 
     render() {
-        console.log(this.state.date)
         return (
-            <div class="listBlock">
-                <div class={' list ' + (this.state.important == 'Y' ? ' important ' : '')} 
-                        ref = {this.list}>
+            <div class={' list ' + (this.state.important == 'Y' ? ' important ' : '')} >
 
-                    <input type="checkbox" class="taskChk" 
-                            checked={this.state.complete} 
-                            onChange={this.tagComplete} />
-                            
-                    <input type="text" 
-                            class={' taskTitle ' + 
-                                    (this.state.complete ? ' complete ' : '') +
-                                    (this.state.important ? ' important ' : '') }
-                            value={this.state.name}  />
+                <input type="checkbox" class="taskChk"
+                    checked={this.state.complete}
+                    onChange={this.tagComplete} />
 
-                    <i class={this.state.important == 'Y' ? 
-                            ' fas fa-star fa-lg iconImportant icon' : ' far fa-star fa-lg icon'}
-                            onClick={this.tagImportant}></i>
+                <input type="text"
+                    class={' taskTitle ' +
+                        (this.state.complete ? ' complete ' : '') +
+                        (this.state.important ? ' important ' : '')}
+                    value={this.state.name} />
 
-                    <i class="fas fa-pen fa-lg icon"></i>
-                    <div class="listIcon">
-                        {this.state.date != '' ? 
-                        <i class="far fa-calendar-alt icon"></i>: ''}
-                        {this.state.date != '' ?
-                        ` ${this.state.date.substring(5).replace('-','/')} ` : ''}
-                        
-                        {this.state.file != '' ? 
+                <i class={this.state.important == 'Y' ?
+                    ' fas fa-star fa-lg iconImportant icon' : ' far fa-star fa-lg icon'}
+                    onClick={this.tagImportant}></i>
+
+                <i class="fas fa-pen fa-lg icon"></i>
+                <div class="listIcon">
+                    {this.state.date != '' ?
+                        <i class="far fa-calendar-alt icon"></i> : ''}
+                    {this.state.date != '' ?
+                        ` ${this.state.date.substring(5).replace('-', '/')} ` : ''}
+
+                    {this.state.file != '' ?
                         <i class="fas fa-file icon"></i> : ''}
 
-                        {this.state.commit != '' ? 
+                    {this.state.commit != '' ?
                         <i class="far fa-comment-dots icon"></i> : ''}
-                    </div>
                 </div>
             </div>
         )
